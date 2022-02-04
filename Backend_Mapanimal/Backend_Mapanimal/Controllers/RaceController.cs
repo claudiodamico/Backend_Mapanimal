dc3de7c82de40850ee98b7d68e9c50729a55647b
claudiodamico <cdamico18@gmail.com>
2022-02-04 19:25:56 -0300

    Initial commit

---
+++ Backend_Mapanimal/Backend_Mapanimal/Controllers/RaceController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Backend_Mapanimal.Controllers
+{
+
+    [Route("api/race")]
+    public class RaceController
+    {
+        [HttpGet]
+        public void Get() // Get general
+        {
+            //return GetAllRaces();
+        }
+
+        [HttpGet("{Id}")] // Get by Id
+        public void Get(int Id)
+        {
+            //return Get();
+        }
+
+        [HttpPost]
+        public void Post()
+        {
+            //return Post();
+        }
+
+        [HttpPut]
+        public void Put()
+        {
+            //return Put();
+        }
+
+        [HttpDelete]
+        public void Delete()
+        {
+            //return Delete();
+        }
+    }
+}
