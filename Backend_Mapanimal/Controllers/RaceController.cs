4fa0265a33521e4aa67fe4ab7798388ebb5330ec
claudiodamico <cdamico18@gmail.com>
2022-02-13 14:49:28 -0300

    Subiendo archivos

---
+++ Backend_Mapanimal/Controllers/RaceController.cs
@@ -0,0 +1,51 @@
+﻿using Backend_Mapanimal.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+
+namespace Backend_Mapanimal.Controllers
+{
+
+    [Route("api/race")]
+    [ApiController]
+    public class RaceController : ControllerBase
+    {
+        private readonly ILogger<RaceController> logger;
+
+        public RaceController(ILogger<RaceController> logger)
+        {
+            this.logger = logger;
+        }
+
+        [HttpGet]
+        public ActionResult <List<Race>> Get()// Get general
+        {
+            return new List<Race>() { new Race() { Id = 1, Name = "Puqui" } };
+        }
+
+        [HttpGet("{Id:int}")] // Get by Id
+        public ActionResult<Race> Get(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost]
+        public ActionResult Post([FromBody] Race race)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPut]
+        public ActionResult Put([FromBody] Race race)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpDelete]
+        public AcceptedResult Delete()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
