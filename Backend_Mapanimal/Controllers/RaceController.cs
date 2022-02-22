2c8402bc1f3e7d41baf4bd56c99b8f7c7a721e16
claudiodamico <cdamico18@gmail.com>
2022-02-22 14:26:14 -0300

    Uploading changes

---
+++ Backend_Mapanimal/Controllers/RaceController.cs
@@ -0,0 +1,52 @@
+﻿using Backend_Mapanimal.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Backend_Mapanimal.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RaceController : ControllerBase
+    {
+        private readonly ILogger<RaceController> _logger;
+
+        public RaceController(ILogger<RaceController> logger)
+        {
+            this._logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<Race>> Get()// Get general
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpGet("{Id:int}")] // Get by Id
+        public async Task <ActionResult<Race>> Get(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post([FromBody] Race race)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<Race>> Put([FromBody] Race race)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpDelete]
+        public async Task<ActionResult<Race>> Delete()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Backend_Mapanimal/Entities/Race.cs
@@ -0,0 +1,9 @@
+﻿namespace Backend_Mapanimal.Entities
+{
+    public class Race
+    {
+        private int Id { get; set; }
+
+        private string Name { get; set; }
+    }
+}
